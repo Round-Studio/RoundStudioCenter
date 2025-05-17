f6c8449615238c7334d4f6cf62921cfd766c830a
祺松 龙 <Longqisong_135@outlook.com>
2025-05-17 21:33:44 +0800

    feat:广告

---
+++ Round.NET.AvaloniaApp.RoundStudioCenter/Views/Controls/FlipView.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Round.NET.AvaloniaApp.RoundStudioCenter.Views.Controls;
+
+public partial class FlipView : UserControl
+{
+    public FlipView()
+    {
+        InitializeComponent();
+    }
+
+    private void Back_OnClick(object? sender, RoutedEventArgs e)
+    {
+    }
+
+    private void Forward_OnClick(object? sender, RoutedEventArgs e)
+    {
+    }
+}
