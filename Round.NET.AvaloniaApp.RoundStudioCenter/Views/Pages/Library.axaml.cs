71835c5325dc3fd09b49a192d6a798efd41c8111
祺松 龙 <Longqisong_135@outlook.com>
2025-06-09 21:06:45 +0800

    feat:添加大量页面

---
+++ Round.NET.AvaloniaApp.RoundStudioCenter/Views/Pages/Library.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Round.NET.AvaloniaApp.RoundStudioCenter.Views.Pages;
+
+public partial class Library : UserControl
+{
+    public Library()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Round.NET.AvaloniaApp.RoundStudioCenter/Views/Pages/Settings.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Round.NET.AvaloniaApp.RoundStudioCenter.Views.Pages;
+
+public partial class Settings : UserControl
+{
+    public Settings()
+    {
+        InitializeComponent();
+    }
+}
